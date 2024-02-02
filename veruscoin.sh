3f83b484b376dcb4994aa6be0270848a4317f0ba
TidoNmac <158553634+TidoNmac@users.noreply.github.com>
2024-02-02 15:48:07 +0700

    veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
 sudo apt update -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
-tar -xvf hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_linux64.tar.gz
 ./hellminer -c stratum+tcp://eu.luckpool.net:3957 -u RLBpgHEW2R1XxoG5meK6mA82BbXCRCLWGr.pepe -p x --cpu 4
